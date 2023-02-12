c09e936507c9542defa2f5e411714ef0bd3bf392
Javokhir <jtursunboev@bk.ru>
2023-02-12 10:29:46 +0900

    sidebar is done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,7 +5,11 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import {Button} from "@mui/material";
 
 
 function Sidebar() {
@@ -14,9 +18,19 @@ function Sidebar() {
       {/* Twitter icon */}
       <TwitterIcon />
 
-      <SidebarOption Icon={HomeIcon} text="Home" />
+      <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Contact" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Categories" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+      {/* Tweet */}
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
+
       {/* SidebarOption */}
       {/* SidebarOption */}
       {/* SidebarOption */}
