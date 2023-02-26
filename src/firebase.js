9cede546271a96c583024c82685445d34a804e73
Javokhir <jtursunboev@bk.ru>
2023-02-26 13:22:20 +0900

    firebase find bugs

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyAUf0y8KrliCU_jc633vBp2KJKzDqC3S3E",
   authDomain: "twitter-clone-2e8b4.firebaseapp.com",
   projectId: "twitter-clone-2e8b4",
   storageBucket: "twitter-clone-2e8b4.appspot.com",
   messagingSenderId: "712079302056",
-  appId: "1:712079302056:web:1655a1e5db67d42fec519d",
-  measurementId: "G-W0Y0458VTQ",
+  appId: "1:712079302056:web:3b32d8d4bd2066c9ec519d",
+  measurementId: "G-Z538V2BMTR",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
+
 export default db;
